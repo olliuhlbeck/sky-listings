91f10218ce45dca3bdd05c10d0adf34abafde805
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-04-29 15:59:06 +0300

    add header and main outlet content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import HeaderContainer from '../components/HeaderComponents/HeaderContainer';
+import background from './assets/background.jpg';
+
 function App() {
   return (
-    <div className='bg-gray-50 w-full h-screen text-center'>
-      <h1 className='text-gray-800'>Temporary header</h1>
+    <div
+      className='w-full min-h-screen text-center font-mono bg-cover bg-center'
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <HeaderContainer title='Coding practice' link='home' />
+      <main className='pt-18 h-80'>
+        <Outlet />
+      </main>
     </div>
   );
 }
