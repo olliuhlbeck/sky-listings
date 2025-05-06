c8b1d532b2f6b88d2333249fc3a7c6eae27387c2
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-05-06 16:12:10 +0300

    edit responsivity and and errors for user

--- frontend/components/LoginComponents/LoginForm.tsx
+++ frontend/components/LoginComponents/LoginForm.tsx
@@ -3,26 +3,48 @@ import { RiLockPasswordLine } from 'react-icons/ri';
 import { FaRegUser } from 'react-icons/fa';
 import IconComponent from '../GeneralComponents/IconComponent';
 import InputField from '../GeneralComponents/InputField';
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
 import { LoginComponentProps } from '../../types/LoginComponentProps';
 import { ActionType } from '../../types/ActionType';
+import { LoginInputs } from '../../types/LoginInputs';
 
 const LoginForm = ({ action, setAction }: LoginComponentProps) => {
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
+  const [inputs, setInputs] = useState<LoginInputs>({
+    email: '',
+    username: '',
+    password: '',
+  });
+  const [errors, setErrors] = useState<{
+    username?: string;
+    email?: string;
+    password?: string;
+  }>({});
 
-  const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const input = event.target.value;
-    setUsername(input);
+  const handleUserNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs({ ...inputs, username: event.target.value });
+    setErrors((prev) => ({ ...prev, username: '' }));
   };
-  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const input = event.target.value;
-    setPassword(input);
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs({ ...inputs, email: event.target.value });
+    setErrors((prev) => ({ ...prev, email: '' }));
   };
-  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const input = event.target.value;
-    setEmail(input);
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs({ ...inputs, password: event.target.value });
+    setErrors((prev) => ({ ...prev, password: '' }));
+  };
+
+  const validateEmail = (email: string) => {
+    // Basic email validation regex copied from internet
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return emailRegex.test(email) ? '' : 'Please enter a valid email.';
+  };
+
+  const validateUsername = (username: string) => {
+    return username.trim().length > 0 ? '' : 'Username is required.';
+  };
+
+  const validatePassword = (password: string) => {
+    return password.length >= 6 ? '' : 'Min. length 6 characters.';
   };
 
   const switchAction = () => {
@@ -35,7 +57,25 @@ const LoginForm = ({ action, setAction }: LoginComponentProps) => {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    const emailError =
+      action === ActionType.SignUp ? validateEmail(inputs.email) : '';
+    const usernameError = validateUsername(inputs.username);
+    const passwordError = validatePassword(inputs.password);
+
+    if (emailError || usernameError || passwordError) {
+      setErrors({
+        email: emailError,
+        username: usernameError,
+        password: passwordError,
+      });
+      return;
+    }
+
+    console.log('Form submitted successfully');
   };
+
+  const isFormValid = !errors.username && !errors.email && !errors.password;
 
   return (
     <form
@@ -43,50 +83,90 @@ const LoginForm = ({ action, setAction }: LoginComponentProps) => {
       onSubmit={handleSubmit}
       className='flex flex-col grow items-center space-y-4'
     >
+      <div className='md:hidden w-full flex items-center justify-center'>
+        <h2 className='text-2xl lg:text-3xl font-bold'>
+          {action === ActionType.Login ? 'Login' : 'Sign up'}
+        </h2>
+      </div>
       {action === ActionType.SignUp && (
-        <div className='flex space-x-4'>
-          <IconComponent icon={MdOutlineEmail} className='self-center' />
-          <InputField
-            name='emailInput'
-            type='text'
-            value={email}
-            onChange={(event) => handleEmailChange(event)}
-            placeholder='Email'
-            className='w-24 md:w-52'
-          />
+        <div className='flex flex-col space-y-1'>
+          <div className='flex space-x-2 items-center'>
+            <IconComponent
+              icon={MdOutlineEmail}
+              className='self-center'
+              aria-hidden='true'
+            />
+            <InputField
+              name='emailInput'
+              type='text'
+              value={inputs.email}
+              onChange={handleEmailChange}
+              placeholder='Email'
+              className='w-52'
+            />
+          </div>
+          {errors.email && (
+            <span className='text-red-500 text-sm'>{errors.email}</span>
+          )}
         </div>
       )}
 
-      <div className='flex space-x-4'>
-        <IconComponent icon={FaRegUser} className='self-center' />
-        <InputField
-          name='usernameInput'
-          type='text'
-          value={username}
-          onChange={(event) => handleUsernameChange(event)}
-          placeholder='Username'
-          className='w-24 md:w-52'
-        />
+      <div className='flex flex-col space-y-1'>
+        <div className='flex space-x-2 items-center'>
+          <IconComponent
+            icon={FaRegUser}
+            className='self-center'
+            aria-hidden='true'
+          />
+          <InputField
+            name='usernameInput'
+            type='text'
+            value={inputs.username}
+            onChange={handleUserNameChange}
+            placeholder='Username'
+            className='w-52'
+          />
+        </div>
+        {errors.username && (
+          <span className='text-red-500 text-sm'>{errors.username}</span>
+        )}
       </div>
-      <div className='flex space-x-4'>
-        <IconComponent icon={RiLockPasswordLine} className='self-center' />
-        <InputField
-          name='passwordInput'
-          type='password'
-          value={password}
-          onChange={(event) => handlePasswordChange(event)}
-          placeholder='Password'
-          className='w-24 md:w-52'
-        />
+
+      <div className='flex flex-col space-y-1'>
+        <div className='flex space-x-2 items-center'>
+          <IconComponent
+            icon={RiLockPasswordLine}
+            className='self-center'
+            aria-hidden='true'
+          />
+          <InputField
+            name='passwordInput'
+            type='password'
+            value={inputs.password}
+            onChange={handlePasswordChange}
+            placeholder='Password'
+            className='w-52'
+          />
+        </div>
+        {errors.password && (
+          <span className='text-red-500 text-sm'>{errors.password}</span>
+        )}
       </div>
-      <button className='hover:cursor-pointer border p-2 rounded-md hover:bg-sky-200'>
-        Login
+
+      <button
+        className='hover:cursor-pointer border p-2 rounded-md hover:bg-sky-200'
+        type='submit'
+        disabled={!isFormValid}
+      >
+        {action === ActionType.Login ? 'Login' : 'Sign Up'}
       </button>
-      <span>
+
+      <div>
         {action === ActionType.Login
           ? 'Do not have an account?'
           : 'Already have a account?'}
         <button
+          type='button'
           className='hover:cursor-pointer hover:bg-sky-200 p-2 rounded-full'
           onClick={switchAction}
         >
@@ -94,7 +174,7 @@ const LoginForm = ({ action, setAction }: LoginComponentProps) => {
             ? 'Create one by clicking here'
             : 'Switch to login by clicking here'}
         </button>
-      </span>
+      </div>
     </form>
   );
 };
