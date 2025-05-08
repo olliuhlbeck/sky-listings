04943b5fc32cbde691da4a26a3a32e51db34765b
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-05-08 15:25:14 +0300

    add login validation middleware

--- backend/src/middlewares/signupAndLogin/loginValidate.ts
+++ backend/src/middlewares/signupAndLogin/loginValidate.ts
@@ -4,23 +4,26 @@ const loginValidate = (req: Request, res: Response, next: NextFunction) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
-    return res.status(400).json({
+    res.status(400).json({
       error: 'Username and password are both needed to login.',
     });
+    return;
   }
 
   if (typeof username !== 'string' || username.length > 50) {
-    return res.status(400).json({
+    res.status(400).json({
       error:
         'Username must be of type string with maximum length of 50 characters.',
     });
+    return;
   }
 
   if (typeof password !== 'string' || password.length > 50) {
-    return res.status(400).json({
+    res.status(400).json({
       error:
         'Password must be of type string with maximum length of 50 characters.',
     });
+    return;
   }
 
   next();
