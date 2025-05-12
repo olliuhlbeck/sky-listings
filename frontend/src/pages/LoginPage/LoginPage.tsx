a9e8c8c5ca8f5d0d1ef0c0a506b45090a417ace6
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-05-12 12:47:46 +0300

    move pages under source folder

---
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import LoginForm from '../../components/LoginComponents/LoginForm';
+import LoginPageTitle from '../../components/LoginComponents/LoginPageTitle';
+import { ActionType } from '../../types/ActionType';
+
+const LoginPage = () => {
+  const [action, setAction] = useState<ActionType>(ActionType.Login);
+
+  return (
+    <div className='flex items-center rounded-lg bg-gray-50 w-5/6 md:w-4/6 min-w-[256px] h-[500px] shadow-md shadow-gray-500 mt-16 mx-auto gap-4'>
+      <LoginForm action={action} setAction={setAction} />
+      <LoginPageTitle action={action} setAction={setAction} />
+    </div>
+  );
+};
+
+export default LoginPage;
