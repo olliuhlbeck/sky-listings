ce6cb0e562cd72350661bacd3e6d8fd9e02c936f
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-05-15 14:13:38 +0300

    add responsivity to user login

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,17 @@
+import { useAuth } from '../../utils/useAuth';
+
 const HomePage = () => {
+  const { user } = useAuth();
   return (
     <div className='flex flex-col justify-center items-center mt-6'>
-      <h1 className='text-3xl'>Welcome!</h1>
+      {user ? (
+        <h1 className='text-3xl'>{`Welcome ${user}!`}</h1>
+      ) : (
+        <>
+          <h1 className='text-3xl mb-4'>Welcome!</h1>
+          <p>Please login to unlock more features</p>
+        </>
+      )}
     </div>
   );
 };
