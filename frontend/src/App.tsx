834a6e292d5deab92b0f98883afdec698f654eb1
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-05-16 17:05:26 +0300

    edit page title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <div
       className={`flex flex-col w-full min-h-screen text-center font-mono bg-cover bg-center bg-[url('./assets/background.png')] text-slate-900`}
     >
-      <HeaderContainer title='Coding practice' link='home' />
+      <HeaderContainer title='Sky Listings' link='home' />
       <main className='flex-grow pt-18'>
         <Outlet />
       </main>
