c8b7f2aae97e75d4f90c0273788d0f93d714f809
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-05-19 15:48:18 +0300

    add responsivity and buttons

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
-import IconComponent from '../../components/GeneralComponents/IconComponent';
-import { useAuth } from '../../utils/useAuth';
 import { BiDollar } from 'react-icons/bi';
+import AdComponent from '../../components/GeneralComponents/AdComponent';
+import { useAuth } from '../../utils/useAuth';
 
 const HomePage = () => {
   const { user } = useAuth();
@@ -8,13 +8,21 @@ const HomePage = () => {
     <div className='flex flex-col justify-center items-center mt-6'>
       {user ? (
         <>
-          <h1 className='text-3xl my-2'>{`Welcome ${user}!`}</h1>
-          <div className='flex gap-4 w-5/6'>
-            <div className='min-h-[20rem] flex flex-1 justify-center items-center'>
-              <h2>Browse houses</h2>
+          <h1 className='text-3xl my-3'>{`Welcome ${user}!`}</h1>
+          <div className='flex flex-col lg:flex-row w-4/6 gap-6'>
+            <div
+              className={`min-h-[12rem] lg:min-h-[24rem] shadow-lg shadow-slate-500 flex flex-1 justify-center items-center rounded-lg bg-[url('./assets/browseHouses.jpg')] bg-cover bg-center`}
+            >
+              <button className='text-2xl bg-sky-200 text-slate-900 rounded-full p-2 hover:bg-sky-400 hover:cursor-pointer'>
+                Search houses
+              </button>
             </div>
-            <div className='min-h-[20rem] flex flex-1 justify-center items-center'>
-              <h2>List your house for sale</h2>
+            <div
+              className={`min-h-[12rem] lg:min-h-[24rem] shadow-lg shadow-slate-500 flex flex-1 justify-center items-center rounded-lg bg-[url('./assets/sellHouse.jpg')] bg-cover bg-center`}
+            >
+              <button className='text-2xl bg-sky-200 text-slate-900 rounded-full p-2 hover:bg-sky-400 hover:cursor-pointer'>
+                List your house for sale
+              </button>
             </div>
           </div>
         </>
@@ -22,7 +30,7 @@ const HomePage = () => {
         <>
           <h1 className='text-3xl my-2'>Welcome!</h1>
           <div className='flex gap-4 w-5/6'>
-            <div className='min-h-[20rem] flex flex-1 justify-center items-center'>
+            <div className='min-h-[20rem] flex flex-1 justify-center items-center '>
               <h2>Browse houses</h2>
             </div>
             <div className='min-h-[20rem] flex flex-1 justify-center items-center'>
@@ -31,30 +39,13 @@ const HomePage = () => {
           </div>
         </>
       )}
-      <div className='flex w-4/6 items-center p-4 rounded-md shadow-lg shadow-slate-500 bg-sky-800 gap-10'>
-        <div className='pl-6 hidden md:block'>
-          <IconComponent icon={BiDollar} size={36} className='text-sky-200' />
-        </div>
-        <div className='w-full'>
-          <h1 className='text-gray-50 font-bold text-2xl'>Mortage Masters</h1>
-          <p className='text-gray-50 my-4'>
-            Short on liquid assets? Mortage Masters can solve your problems!
-          </p>
-          <a
-            href='#'
-            onClick={(e) => {
-              e.preventDefault();
-              console.log('Dummy advertisement.');
-            }}
-            className='inline-block border-none p-2 rounded-full w-content md:w-3xs bg-sky-200 hover:bg-sky-300 hover:cursor-pointer text-center'
-          >
-            Apply for loan
-          </a>
-        </div>
-        <div className='pr-6 hidden md:block'>
-          <IconComponent icon={BiDollar} size={36} className='text-sky-200' />
-        </div>
-      </div>
+      <AdComponent
+        title='Mortgage Masters'
+        message='Short on liquid assets? Mortgage Masters can solve your problems!'
+        buttonText='Apply for loan'
+        icon={BiDollar}
+        addToClassName='mt-6'
+      />
     </div>
   );
 };
