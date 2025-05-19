e3facf44079ddda6e256649f4bbe5165583c0ac9
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-05-19 13:07:12 +0300

    Add dummy add to front page

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,6 @@
+import IconComponent from '../../components/GeneralComponents/IconComponent';
 import { useAuth } from '../../utils/useAuth';
+import { BiDollar } from 'react-icons/bi';
 
 const HomePage = () => {
   const { user } = useAuth();
@@ -8,10 +10,10 @@ const HomePage = () => {
         <>
           <h1 className='text-3xl my-2'>{`Welcome ${user}!`}</h1>
           <div className='flex gap-4 w-5/6'>
-            <div className='flex flex-1 justify-center items-center'>
+            <div className='min-h-[20rem] flex flex-1 justify-center items-center'>
               <h2>Browse houses</h2>
             </div>
-            <div className='flex flex-1 justify-center items-center'>
+            <div className='min-h-[20rem] flex flex-1 justify-center items-center'>
               <h2>List your house for sale</h2>
             </div>
           </div>
@@ -20,15 +22,39 @@ const HomePage = () => {
         <>
           <h1 className='text-3xl my-2'>Welcome!</h1>
           <div className='flex gap-4 w-5/6'>
-            <div className='flex flex-1 justify-center items-center'>
+            <div className='min-h-[20rem] flex flex-1 justify-center items-center'>
               <h2>Browse houses</h2>
             </div>
-            <div className='flex flex-1 justify-center items-center'>
+            <div className='min-h-[20rem] flex flex-1 justify-center items-center'>
               <h2>List your house for sale</h2>
             </div>
           </div>
         </>
       )}
+      <div className='flex w-4/6 items-center p-4 rounded-md shadow-lg shadow-slate-500 bg-sky-800 gap-10'>
+        <div className='pl-6 hidden md:block'>
+          <IconComponent icon={BiDollar} size={36} className='text-sky-200' />
+        </div>
+        <div className='w-full'>
+          <h1 className='text-gray-50 font-bold text-2xl'>Mortage Masters</h1>
+          <p className='text-gray-50 my-4'>
+            Short on liquid assets? Mortage Masters can solve your problems!
+          </p>
+          <a
+            href='#'
+            onClick={(e) => {
+              e.preventDefault();
+              console.log('Dummy advertisement.');
+            }}
+            className='inline-block border-none p-2 rounded-full w-content md:w-3xs bg-sky-200 hover:bg-sky-300 hover:cursor-pointer text-center'
+          >
+            Apply for loan
+          </a>
+        </div>
+        <div className='pr-6 hidden md:block'>
+          <IconComponent icon={BiDollar} size={36} className='text-sky-200' />
+        </div>
+      </div>
     </div>
   );
 };
