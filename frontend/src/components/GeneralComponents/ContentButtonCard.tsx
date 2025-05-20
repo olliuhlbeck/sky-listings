0214f94e69385bc8db6a10647179cc938ccb915b
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-05-20 15:10:49 +0300

    add responsivity

--- frontend/src/components/GeneralComponents/ContentButtonCard.tsx
+++ frontend/src/components/GeneralComponents/ContentButtonCard.tsx
@@ -8,7 +8,7 @@ const ContentButtonCard = ({
     <div
       className={`min-h-[12rem] lg:min-h-[24rem] shadow-lg shadow-slate-500 flex flex-1 justify-center items-center rounded-lg bg-[url('./assets/${backgroundImage}')] bg-cover bg-center`}
     >
-      <button className='text-2xl bg-sky-200 text-slate-900 rounded-md p-2 hover:bg-sky-400 hover:cursor-pointer'>
+      <button className='w-[16rem] md:w-[20rem] text-2xl bg-sky-200 text-slate-900 rounded-md p-2 hover:bg-sky-400 hover:cursor-pointer'>
         {buttonText}
       </button>
     </div>
