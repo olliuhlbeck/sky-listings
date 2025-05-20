dd8670e8d24493cbc41d2f09e209d499be96dc42
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-05-20 14:33:52 +0300

    add check for passed ActionType

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -2,9 +2,14 @@ import { useState } from 'react';
 import LoginForm from '../../components/LoginComponents/LoginForm';
 import LoginPageTitle from '../../components/LoginComponents/LoginPageTitle';
 import { ActionType } from '../../types/ActionType';
+import { useLocation } from 'react-router-dom';
 
 const LoginPage = () => {
-  const [action, setAction] = useState<ActionType>(ActionType.Login);
+  const location = useLocation();
+  const initialAction =
+    (location.state?.action as ActionType) || ActionType.Login;
+
+  const [action, setAction] = useState<ActionType>(initialAction);
 
   return (
     <div className='flex items-center rounded-lg bg-gray-50 w-5/6 md:w-4/6 min-w-[256px] h-[500px] shadow-md shadow-gray-500 mt-16 mx-auto gap-4'>
