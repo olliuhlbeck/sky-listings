5814242a54d3b344eadcaef4420daafedcd03f6a
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-05-20 14:39:12 +0300

    add content for not logged in user

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,26 +1,30 @@
 import { BiDollar } from 'react-icons/bi';
 import AdComponent from '../../components/GeneralComponents/AdComponent';
 import { useAuth } from '../../utils/useAuth';
+import { useNavigate } from 'react-router-dom';
+import { ActionType } from '../../types/ActionType';
 
 const HomePage = () => {
   const { user } = useAuth();
+  const navigate = useNavigate();
+
   return (
     <div className='flex flex-col justify-center items-center mt-6'>
       {user ? (
         <>
-          <h1 className='text-3xl my-3'>{`Welcome ${user}!`}</h1>
+          <h1 className='text-3xl my-3'>{`Hi ${user}!`}</h1>
           <div className='flex flex-col lg:flex-row w-4/6 gap-6'>
             <div
               className={`min-h-[12rem] lg:min-h-[24rem] shadow-lg shadow-slate-500 flex flex-1 justify-center items-center rounded-lg bg-[url('./assets/browseHouses.jpg')] bg-cover bg-center`}
             >
-              <button className='text-2xl bg-sky-200 text-slate-900 rounded-full p-2 hover:bg-sky-400 hover:cursor-pointer'>
+              <button className='text-2xl bg-sky-200 text-slate-900 rounded-md p-2 hover:bg-sky-400 hover:cursor-pointer'>
                 Search houses
               </button>
             </div>
             <div
               className={`min-h-[12rem] lg:min-h-[24rem] shadow-lg shadow-slate-500 flex flex-1 justify-center items-center rounded-lg bg-[url('./assets/sellHouse.jpg')] bg-cover bg-center`}
             >
-              <button className='text-2xl bg-sky-200 text-slate-900 rounded-full p-2 hover:bg-sky-400 hover:cursor-pointer'>
+              <button className='text-2xl bg-sky-200 text-slate-900 rounded-md p-2 hover:bg-sky-400 hover:cursor-pointer'>
                 List your house for sale
               </button>
             </div>
@@ -28,13 +32,26 @@ const HomePage = () => {
         </>
       ) : (
         <>
-          <h1 className='text-3xl my-2'>Welcome!</h1>
+          <h1 className='text-3xl my-2'>Welcome to Sky Listings!</h1>
           <div className='flex gap-4 w-5/6'>
-            <div className='min-h-[20rem] flex flex-1 justify-center items-center '>
-              <h2>Browse houses</h2>
+            <div
+              className={`min-h-[12rem] lg:min-h-[24rem] shadow-lg shadow-slate-500 flex flex-1 justify-center items-center rounded-lg bg-[url('./assets/browseHouses.jpg')] bg-cover bg-center`}
+            >
+              <button className='text-2xl bg-sky-200 text-slate-900 rounded-md p-2 hover:bg-sky-400 hover:cursor-pointer'>
+                Search houses
+              </button>
             </div>
-            <div className='min-h-[20rem] flex flex-1 justify-center items-center'>
-              <h2>List your house for sale</h2>
+            <div
+              className={`min-h-[12rem] lg:min-h-[24rem] shadow-lg shadow-slate-500 flex flex-1 justify-center items-center rounded-lg bg-[url('./assets/registerFront.png')] bg-cover bg-center`}
+            >
+              <button
+                className='text-2xl bg-sky-200 text-slate-900 rounded-md p-2 hover:bg-sky-400 hover:cursor-pointer'
+                onClick={() =>
+                  navigate('login', { state: { action: ActionType.SignUp } })
+                }
+              >
+                Register to list your property!
+              </button>
             </div>
           </div>
         </>
