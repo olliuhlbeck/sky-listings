a3b0cf9d85aeb8d8ac945bcc46a08373b707b081
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-05-20 14:33:11 +0300

    add action state to interface

--- frontend/src/types/HeaderButtonProps.ts
+++ frontend/src/types/HeaderButtonProps.ts
@@ -1,9 +1,11 @@
 import { MouseEventHandler } from 'react';
 import { IconType } from 'react-icons';
+import { ActionType } from './ActionType';
 
 export interface HeaderButtonProps {
   text: string;
   link: string;
   onClick?: MouseEventHandler<HTMLAnchorElement>;
   icon?: IconType;
+  state?: { action: ActionType };
 }
