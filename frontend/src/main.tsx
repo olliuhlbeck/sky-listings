25fc8102114a89bae836bcd34058fcc35087537d
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-05-21 12:54:54 +0300

    add routes to property content

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { router } from '../routes/Routes.tsx';
+import { router } from '../src/routes/Routes.tsx';
 import { RouterProvider } from 'react-router-dom';
 import AuthProvider from './components/AuthComponents/AuthProvider.tsx';
 
