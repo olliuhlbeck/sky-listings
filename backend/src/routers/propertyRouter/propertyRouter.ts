8332dfcdd2725af4cfb3014cca11c13a0681f8b6
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-05-28 16:19:50 +0300

    add part of logic

--- backend/src/routers/propertyRouter/propertyRouter.ts
+++ backend/src/routers/propertyRouter/propertyRouter.ts
@@ -2,6 +2,11 @@ import express, { Request, Response } from 'express';
 import { PrismaClient } from '../../../generated/prisma';
 import { CreatePropertyDTO } from '../../types/dtos/CreateProperty.dto';
 import propertyCreationValidate from '../../middlewares/property/propertyCreationValidate';
+import {
+  madDtoToPrismaEnumAddPropertyType,
+  madDtoToPrismaEnumAddPropertyStatus,
+} from '../../utils/mapDtoToPrismaEnumAddProperty';
+
 const propertyRouter = express.Router();
 const prisma = new PrismaClient();
 
@@ -20,16 +25,61 @@ propertyRouter.post(
       propertyStatus,
       bedrooms,
       bathrooms,
-      squaremeters,
+      squareMeters,
       description,
       additionalInfo,
       pictures,
+      coverPictureIndex,
     } = req.body;
 
     try {
       console.log('trying to create property with:', req.body);
+
+      const createdProperty = await prisma.property.create({
+        data: {
+          userId: 2,
+          bedrooms: bedrooms,
+          bathrooms: bathrooms,
+          additionalInfo: additionalInfo,
+          propertyType: madDtoToPrismaEnumAddPropertyType(propertyType),
+          description: description,
+          city: city,
+          country: country,
+          postalCode: postalCode,
+          price: price,
+          propertyStatus: madDtoToPrismaEnumAddPropertyStatus(propertyStatus),
+          squareMeters: squareMeters,
+          state: state,
+          street: street,
+        },
+      });
+      console.log('Property created:', createdProperty);
+
+      if (pictures && pictures.length > 0) {
+        const pictureData = pictures.map(
+          (pictureBase64: string, index: number) => ({
+            propertyId: createdProperty.id,
+            picture: Buffer.from(pictureBase64, 'base64'),
+            useAsCoverPicture: index === coverPictureIndex,
+          }),
+        );
+
+        await prisma.propertyPicture.createMany({
+          data: pictureData,
+        });
+
+        console.log('Pictures created for property:', createdProperty.id);
+      }
+
+      res.status(201).json({
+        message: 'Property and its pictures stored successfully',
+        propertyId: createdProperty.id,
+      });
     } catch (error) {
-      console.error('error in propert creating', error);
+      console.error('error in property creating', error);
+      res.status(500).json({ error: 'Failed to create property' });
+    } finally {
+      console.log('property creation has ended.');
     }
   },
 );
