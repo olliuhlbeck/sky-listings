24beaba71579ccaa8bf07e26c283eccadc93bb08
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-06-06 15:26:27 +0300

    add hamburger menu

--- frontend/src/components/HeaderComponents/HeaderContainer.tsx
+++ frontend/src/components/HeaderComponents/HeaderContainer.tsx
@@ -6,8 +6,16 @@ import { CiLogin, CiLogout } from 'react-icons/ci';
 import { useAuth } from '../../utils/useAuth';
 import { GiFamilyHouse } from 'react-icons/gi';
 import { ActionType } from '../../types/ActionType';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { IoClose } from 'react-icons/io5';
+import { useEffect, useRef } from 'react';
 
-const HeaderContainer = ({ title, link }: HeaderContainerProps) => {
+const HeaderContainer = ({
+  title,
+  link,
+  isHamburgerMenuOpen,
+  setIsHamburgerMenuOpen,
+}: HeaderContainerProps) => {
   const navigationLinks = [
     { text: 'Home', link: 'home', authOnly: 'no' },
     { text: 'Pagination', link: 'pagination', authOnly: 'no' },
@@ -20,6 +28,29 @@ const HeaderContainer = ({ title, link }: HeaderContainerProps) => {
   const location = useLocation();
   const isUserInLoginPage = location.pathname === '/login';
 
+  // Hamburger menu pop up closing
+  const hamburgerRef = useRef<HTMLDivElement>(null);
+  const popupRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        isHamburgerMenuOpen &&
+        hamburgerRef.current &&
+        popupRef.current &&
+        !hamburgerRef.current.contains(event.target as Node) &&
+        !popupRef.current.contains(event.target as Node)
+      ) {
+        setIsHamburgerMenuOpen(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isHamburgerMenuOpen, setIsHamburgerMenuOpen]);
+
   return (
     <header
       className={`fixed top-0 left-0 w-full bg-cover bg-top bg-[url('./assets/background.png')] shadow-sm`}
@@ -29,6 +60,43 @@ const HeaderContainer = ({ title, link }: HeaderContainerProps) => {
           <IconComponent icon={GiFamilyHouse} size={32} />
           <h2 className='text-2xl text-center hidden md:block'>{title}</h2>
         </Link>
+        <div
+          ref={hamburgerRef}
+          className='xl:hidden'
+          onClick={() => setIsHamburgerMenuOpen(!isHamburgerMenuOpen)}
+        >
+          {isHamburgerMenuOpen ? (
+            <IconComponent
+              icon={IoClose}
+              size={30}
+              className='hover:bg-sky-300 transition duration-200 p-1 rounded-md hover:cursor-pointer'
+            />
+          ) : (
+            <IconComponent
+              icon={RxHamburgerMenu}
+              size={30}
+              className='hover:bg-sky-300 transition duration-200 p-1 rounded-md hover:cursor-pointer'
+            />
+          )}
+        </div>
+        {isHamburgerMenuOpen && (
+          <div
+            ref={popupRef}
+            className='absolute top-20 right-1/2 md:left-1/2 translate-x-1/3 transform md:-translate-x-1/3 w-64 bg-white shadow-lg rounded-md p-2 xl:hidden'
+          >
+            {navigationLinks
+              .filter((link) => link.authOnly === 'no' || user !== null)
+              .map(({ text, link }) => (
+                <HeaderButton
+                  additionsToClassName='mt-3'
+                  key={`${link}MobileMenuButton`}
+                  text={text}
+                  link={link}
+                  onClick={() => setIsHamburgerMenuOpen(false)}
+                />
+              ))}
+          </div>
+        )}
         <div className='absolute left-1/2 transform -translate-x-1/2 space-x-2 hidden xl:flex'>
           {navigationLinks
             .filter((link) => link.authOnly === 'no' || user !== null)
