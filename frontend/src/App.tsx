d382b7b2b68c68d931cbf6d9f31ae8a57cb332bf
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-06-06 15:25:51 +0300

    add hamburger menu controls

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,11 +6,17 @@ import Footer from '../src/components/FooterComponents/Footer';
 
 const App = () => {
   const [footerVisible, setFooterVisible] = useState<boolean>(true);
+  const [isHamburgerMenuOpen, setIsHamburgerMenuOpen] = useState<boolean>(true);
   return (
     <div
       className={`flex flex-col w-full min-h-screen text-center font-mono bg-cover bg-center bg-[url('./assets/background.png')] text-slate-900`}
     >
-      <HeaderContainer title='Sky Listings' link='home' />
+      <HeaderContainer
+        title='Sky Listings'
+        link='home'
+        isHamburgerMenuOpen={isHamburgerMenuOpen}
+        setIsHamburgerMenuOpen={setIsHamburgerMenuOpen}
+      />
       <main className='flex-grow pt-18'>
         <Outlet />
       </main>
