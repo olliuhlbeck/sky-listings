ff389169914d8dc019a16643566b71749eff3998
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-06-13 15:34:38 +0300

    remove pagination related content

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import App from '../App';
 import HomePage from '../pages/HomePage/HomePage';
-import Pagination from '../pages/Pagination/Pagination';
 import LoginPage from '../pages/LoginPage/LoginPage';
 import BrowseProperties from '../pages/BrowseProperties/BrowseProperties';
 import MyProperties from '../pages/MyProperties/MyProperties';
@@ -15,7 +14,6 @@ export const router = createBrowserRouter([
     children: [
       { path: '', element: <HomePage /> },
       { path: 'home', element: <Navigate to='/' /> },
-      { path: 'pagination', element: <Pagination /> },
       { path: 'login', element: <LoginPage /> },
       { path: 'browseProperties', element: <BrowseProperties /> },
       { path: 'myProperties', element: <MyProperties /> },
