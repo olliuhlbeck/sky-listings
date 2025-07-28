4591fa557564611c2ca412166abcc16a55c70230
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-07-28 15:13:27 +0300

    add mocks to jest ignore

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -3,6 +3,8 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  coveragePathIgnorePatterns: ['/node_modules/', '/__tests__/__mocks__/'],
+  testPathIgnorePatterns: ['/node_modules/', '/__tests__/__mocks__/'],
 };
 
 export default config;
