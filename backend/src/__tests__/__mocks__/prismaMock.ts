af330cf05ada7e257b3a0193531a7cb44cc29d7e
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-07-30 14:32:50 +0300

    add mocks for prisma property

--- backend/src/__tests__/__mocks__/prismaMock.ts
+++ backend/src/__tests__/__mocks__/prismaMock.ts
@@ -2,12 +2,24 @@ export const mockFindUnique = jest.fn();
 export const mockCreate = jest.fn();
 export const mockUpdate = jest.fn();
 export const mockDelete = jest.fn();
+export const mockCount = jest.fn();
+export const mockFindMany = jest.fn();
 
 export const prismaMock = {
   user: {
-    findUnique: mockFindUnique,
     create: mockCreate,
-    update: mockUpdate,
     delete: mockDelete,
+    findUnique: mockFindUnique,
+    update: mockUpdate,
+  },
+  property: {
+    count: mockCount,
+    create: mockCreate,
+    findMany: mockFindMany,
+    findUnique: mockFindUnique,
+    update: mockUpdate,
+  },
+  propertyPicture: {
+    create: mockCreate,
   },
 };
