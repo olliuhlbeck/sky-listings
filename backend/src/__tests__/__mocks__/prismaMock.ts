d43ba46c47552656e3cd56c70efcdbe0c00e0691
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-08-01 13:08:42 +0300

    isolate mocks per prisma model

--- backend/src/__tests__/__mocks__/prismaMock.ts
+++ backend/src/__tests__/__mocks__/prismaMock.ts
@@ -1,25 +1,24 @@
-export const mockFindUnique = jest.fn();
-export const mockCreate = jest.fn();
-export const mockUpdate = jest.fn();
-export const mockDelete = jest.fn();
-export const mockCount = jest.fn();
-export const mockFindMany = jest.fn();
+export const userMock = {
+  create: jest.fn(),
+  delete: jest.fn(),
+  findUnique: jest.fn(),
+  update: jest.fn(),
+};
+
+export const propertyMock = {
+  count: jest.fn(),
+  create: jest.fn(),
+  findMany: jest.fn(),
+  findUnique: jest.fn(),
+  update: jest.fn(),
+};
+
+export const propertyPictureMock = {
+  create: jest.fn(),
+};
 
 export const prismaMock = {
-  user: {
-    create: mockCreate,
-    delete: mockDelete,
-    findUnique: mockFindUnique,
-    update: mockUpdate,
-  },
-  property: {
-    count: mockCount,
-    create: mockCreate,
-    findMany: mockFindMany,
-    findUnique: mockFindUnique,
-    update: mockUpdate,
-  },
-  propertyPicture: {
-    create: mockCreate,
-  },
+  user: userMock,
+  property: propertyMock,
+  propertyPicture: propertyPictureMock,
 };
