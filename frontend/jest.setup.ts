7a61398570bc12812d1f4e296a4528a1eef24cdb
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-08-07 15:19:27 +0300

    polyfill global TextEncoder and TextDecoder

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -1,1 +1,11 @@
+import { TextEncoder, TextDecoder } from 'util';
+
+if (typeof global.TextEncoder === 'undefined') {
+  global.TextEncoder = TextEncoder as typeof global.TextEncoder;
+}
+
+if (typeof global.TextDecoder === 'undefined') {
+  global.TextDecoder = TextDecoder as unknown as typeof global.TextDecoder;
+}
+
 import '@testing-library/jest-dom';
