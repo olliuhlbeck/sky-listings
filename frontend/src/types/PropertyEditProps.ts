50563c06d0b57e0db9de8923954d7a6c7915c64c
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-08-21 14:29:31 +0300

    remove not needed onFieldEdit

--- frontend/src/types/PropertyEditProps.ts
+++ frontend/src/types/PropertyEditProps.ts
@@ -3,8 +3,4 @@ import { PropertyResponse } from './dtos/PropertyResponse.dto';
 export interface PropertyEditProps {
   property: PropertyResponse;
   originalProperty: PropertyResponse;
-  onFieldEdit: (
-    field: keyof PropertyResponse,
-    value: string | number | null,
-  ) => void;
 }
