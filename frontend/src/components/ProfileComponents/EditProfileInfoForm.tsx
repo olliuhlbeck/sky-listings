53cdb9f743b78bb3c3718416afd0304792503e4f
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-09-08 14:26:28 +0300

    edit styling

--- frontend/src/components/ProfileComponents/EditProfileInfoForm.tsx
+++ frontend/src/components/ProfileComponents/EditProfileInfoForm.tsx
@@ -1,6 +1,6 @@
 const EditProfileInfoForm = () => {
   return (
-    <div className='md:w-2/3 bg-amber-200'>
+    <div className='sm:w-2/3 bg-amber-200'>
       <h2 className=''>Form</h2>
     </div>
   );
