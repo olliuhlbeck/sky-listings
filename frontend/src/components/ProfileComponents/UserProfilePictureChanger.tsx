16b905ced9f86c82333a84e00307abca220d2396
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-09-09 12:51:48 +0300

    widen gap

--- frontend/src/components/ProfileComponents/UserProfilePictureChanger.tsx
+++ frontend/src/components/ProfileComponents/UserProfilePictureChanger.tsx
@@ -5,7 +5,7 @@ import IconComponent from '../GeneralComponents/IconComponent';
 const UserProfilePictureChanger = () => {
   const token = useAuth();
   return (
-    <div className='flex flex-col sm:w-1/3 items-center justify-center gap-1 sm:gap-4'>
+    <div className='flex flex-col sm:w-1/3 items-center justify-center gap-2 sm:gap-4'>
       <IconComponent icon={CgProfile} size={24} />
       <h2>{token.user}</h2>
     </div>
