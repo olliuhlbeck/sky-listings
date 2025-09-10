1b67983f56d8933fe8effaf1561538f1e3035a98
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-09-10 13:39:32 +0300

    add route for fetching user information

--- backend/src/routers/contactInfoRouter/contactInfoRouter.ts
+++ backend/src/routers/contactInfoRouter/contactInfoRouter.ts
@@ -4,6 +4,8 @@ import { GeneralErrorResponse } from '../../types/general-error';
 import {
   ContactInfoReturnDto,
   GetContactInfo,
+  GetUserInfo,
+  UserInfoReturnDto,
 } from '../../types/dtos/GetContactInfo.dto';
 
 const contactInfoRouter = express.Router();
@@ -67,6 +69,73 @@ contactInfoRouter.get(
       res.status(200).json(contactInfo);
     } catch {
       res.status(500).json({ error: 'Error fetching contact info in backend' });
+      return;
+    }
+  },
+);
+
+/*
+ * Get all user info route
+ * -Fetches first name, last name, phone, email and preferred contact method and returns them combined to display in profile settings page
+ */
+contactInfoRouter.get(
+  '/getAllUserInfo',
+  async (
+    req: Request<{}, {}, {}, GetUserInfo>,
+    res: Response<UserInfoReturnDto | GeneralErrorResponse>,
+  ): Promise<void> => {
+    const userId = parseInt(req.query.userId as string, 10);
+    if (!userId || isNaN(userId)) {
+      res.status(400).json({ error: 'Invalid userId' });
+      return;
+    }
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id: userId },
+        include: {
+          info: true,
+        },
+      });
+
+      if (!user) {
+        res.status(404).json({ error: 'User not found' });
+        return;
+      }
+
+      if (!user.info) {
+        res.status(404).json({ error: 'UserInfo not found' });
+        return;
+      }
+
+      if (user?.email === undefined) {
+        res.status(404).json({ error: 'No email found. Please check again' });
+        return;
+      }
+      if (user.info?.preferredContactDetails === undefined) {
+        res.status(404).json({
+          error: 'No preferred contact method found. Please check again',
+        });
+        return;
+      }
+      if (user.info?.phone === undefined) {
+        res.status(404).json({
+          error: 'No phone number found. Please check again',
+        });
+        return;
+      }
+
+      const userInfo: UserInfoReturnDto = {
+        firstName: user.info.firstName,
+        lastName: user.info.lastName,
+        phoneNumber: user.info.phone,
+        email: user.email,
+        preferredContactMethod: user.info.preferredContactDetails,
+      };
+      res.status(200).json(userInfo);
+    } catch {
+      res
+        .status(500)
+        .json({ error: 'Error fetching contact info in backend.' });
       return;
     }
   },
