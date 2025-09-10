be8c6c65213ccb8cc252936f1bdc7f5bd70d459b
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-09-10 11:56:03 +0300

    add not chosen to options for preferred contact style

--- frontend/src/components/ProfileComponents/EditProfileInfoForm.tsx
+++ frontend/src/components/ProfileComponents/EditProfileInfoForm.tsx
@@ -82,6 +82,9 @@ const EditProfileInfoForm = () => {
             onChange={handleSelectChange('preferredContactMethod')}
             className='w-2/3 bg-white px-3 py-2 border border-gray-300 rounded-md dark:text-slate-900 focus:outline-none focus:ring-2 focus:ring-blue-900'
           >
+            <option value='notChosen' className='text-center'>
+              Not chosen
+            </option>
             <option value='email' className='text-center'>
               Email
             </option>
