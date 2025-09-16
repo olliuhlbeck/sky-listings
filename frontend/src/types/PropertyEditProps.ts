197e2efcb989f8c111eabfcce08aaadeb2629cea
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-09-16 13:57:43 +0300

    add callback for property update

--- frontend/src/types/PropertyEditProps.ts
+++ frontend/src/types/PropertyEditProps.ts
@@ -3,4 +3,5 @@ import { UserProperty } from './dtos/GetUsersPropertiesByUserIdResponse';
 export interface PropertyEditProps {
   property: UserProperty;
   originalProperty: UserProperty;
+  onPropertyUpdate: (updated: UserProperty) => void;
 }
