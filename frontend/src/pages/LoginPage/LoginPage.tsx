ea4c7004ea34f90e36f41f7cde1c8505e90bb8ed
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-11-04 17:24:06 +0200

    add test id to main container

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -14,7 +14,10 @@ const LoginPage = () => {
   const [action, setAction] = useState<ActionType>(initialAction);
 
   return (
-    <div className='relative p- bg-gray-50 dark:bg-blue-950 flex items-center rounded-lg w-5/6 md:w-4/6 min-w-[256px] md:h-[620px] shadow-md shadow-gray-700 mt-10 mx-auto gap-4 overflow-hidden'>
+    <div
+      data-testid='login-page-main-container'
+      className='relative bg-gray-50 dark:bg-blue-950 flex items-center rounded-lg w-5/6 md:w-4/6 min-w-[256px] md:h-[620px] shadow-md shadow-gray-700 mt-10 mx-auto gap-4 overflow-hidden'
+    >
       <div className='w-full md:w-auto flex-1 pt-16 pb-2 md:pb-4'>
         <LoginForm action={action} setAction={setAction} />
       </div>
