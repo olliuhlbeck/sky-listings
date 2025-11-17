ca443e9d87eb3992c394ba39e1b44ce6684c6e1c
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-11-17 14:45:35 +0200

    add test ids

--- frontend/src/pages/MyPropertiesPage/MyPropertiesPage.tsx
+++ frontend/src/pages/MyPropertiesPage/MyPropertiesPage.tsx
@@ -103,7 +103,12 @@ const MyProperties = () => {
         {loading && (
           <div className='flex justify-center items-center py-8'>
             <div className='animate-spin rounded-full h-8 w-8 border-b-2 border-blue-500'></div>
-            <p className='ml-3 text-gray-600'>Loading your properties...</p>
+            <p
+              data-testid='loading-properties-text'
+              className='ml-3 text-gray-600'
+            >
+              Loading your properties...
+            </p>
           </div>
         )}
         {/* Error display */}
