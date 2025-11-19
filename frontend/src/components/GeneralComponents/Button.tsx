c72ae8708bedba3cc6c406a00603ce19489c7825
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-11-19 15:44:41 +0200

    add test id handling

--- frontend/src/components/GeneralComponents/Button.tsx
+++ frontend/src/components/GeneralComponents/Button.tsx
@@ -12,6 +12,7 @@ const Button = ({
   iconSize,
   onClick,
   disabled,
+  testId,
 }: ButtonProps) => {
   const hasText = Boolean(children ?? text);
   const basicClassName = `flex items-center ${hasText ? ' gap-2 px-4 py-2 ' : 'p-2'} rounded-md text-xs md:text-base lg:text-lg bg-sky-200 dark:bg-blue-800 hover:cursor-pointer hover:bg-sky-300 dark:hover:bg-blue-700 disabled:cursor-not-allowed disabled:opacity-50 transition duration-200 ${ClassName}`;
@@ -32,6 +33,7 @@ const Button = ({
         className={basicClassName}
         onClick={onClick}
         disabled={disabled}
+        data-testid={testId}
       >
         {icon && <IconComponent icon={icon} size={iconSize} />}
         <span>{children ?? text}</span>
