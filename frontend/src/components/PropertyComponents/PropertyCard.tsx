3ac9df8bd7d8ef7a73fe48600a7984895efd9a4f
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-12-05 15:24:44 +0200

    switch image container to image and wrap it into container with the property type badge

--- frontend/src/components/PropertyComponents/PropertyCard.tsx
+++ frontend/src/components/PropertyComponents/PropertyCard.tsx
@@ -14,11 +14,13 @@ const PropertyCard: React.FC<PropertyCardProps> = ({
 }) => {
   return (
     <div className='rounded-lg overflow-hidden h-84 sm:h-98 min-w-56 shadow-md dark:shadow-none dark:ring-1 dark:ring-slate-600/50 dark:bg-slate-800/80 hover:scale-101 hover:dark:ring-slate-500 transition-all duration-200'>
-      <div
-        className='h-7/10 sm:h-8/10 md:h-8/10 lg:h-8/10 bg-cover bg-center relative'
-        style={{ backgroundImage: `url(${imageUrl})` }}
-        role='img'
-      >
+      <div className='relative h-7/10 sm:h-8/10'>
+        <img
+          src={imageUrl}
+          alt={`${propertyType} in ${street}, ${city}`}
+          className='w-full h-full object-cover'
+        />
+
         <div className='absolute top-3 right-3 bg-gray-50 dark:bg-slate-700 text-slate-900 dark:text-gray-300 bg-opacity-70 px-2 py-1 rounded-full text-sm font-medium'>
           {propertyType}
         </div>
