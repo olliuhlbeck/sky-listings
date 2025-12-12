c975d775ab7a83efe6ec8abf4dfc3a643b1b4d69
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2025-12-12 13:22:55 +0200

    add generated prisma file to coveragePathIgnorePAtterns

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -3,8 +3,16 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  coveragePathIgnorePatterns: ['/node_modules/', '/__tests__/__mocks__/'],
-  testPathIgnorePatterns: ['/node_modules/', '/__tests__/__mocks__/'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/__tests__/__mocks__/',
+    '/generated/prisma',
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/__tests__/__mocks__/',
+    '/generated/prisma',
+  ],
   modulePathIgnorePatterns: ['/dist/'],
 };
 
