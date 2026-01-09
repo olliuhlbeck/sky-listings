7c5673c63e55fed07ae9664b89b2b8aa09e8b9dc
Olli Uhlbeck <olli.uhlbeck@gmail.com>
2026-01-09 17:52:41 +0200

    add preview localhost4173 to cors origins

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ import { env } from './config/env';
 const server: Express = express();
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'http://localhost:4173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
